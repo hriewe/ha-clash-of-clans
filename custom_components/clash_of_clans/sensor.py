2fbe373618bfa35c1507afc0b275bb64eed41199
Hayden Riewe <hriewe12@gmail.com>
2026-01-21 14:01:36 -0500

    add device info

--- custom_components/clash_of_clans/sensor.py
+++ custom_components/clash_of_clans/sensor.py
@@ -1,5 +1,6 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import DOMAIN
 
@@ -24,6 +25,26 @@ class ClashPlayerTrophiesSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_unique_id = f"{coordinator.player_tag}_trophies"
 
+        # Device Info
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, coordinator.player_tag)},
+            name=f"Clash of Clans ({coordinator.player_tag})",
+            manufacturer="Supercell",
+            model="Clash of Clans",
+        )
+
     @property
     def native_value(self):
         return self.coordinator.data["player"]["trophies"]
+    
+    @property
+    def extra_state_attributes(self):
+        player = self.coordinator.data["player"]
+
+        return {
+            "town_hall_level": player.get("townHallLevel"),
+            "xp_level": player.get("expLevel"),
+            "league": player.get("leagueTier", {}).get("name"),
+            "best_trophies": player.get("bestTrophies"),
+            "war_stars": player.get("warStars"),
+        }
