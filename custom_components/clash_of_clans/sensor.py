b6c0be899913012cdf8bacebdd8b47f34b95220a
Hayden Riewe <hriewe12@gmail.com>
2026-01-21 14:42:14 -0500

    add new sensors

--- custom_components/clash_of_clans/sensor.py
+++ custom_components/clash_of_clans/sensor.py
@@ -11,40 +11,140 @@ async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities(
         [
             ClashPlayerTrophiesSensor(coordinator),
+            ClashPlayerBestTrophiesSensor(coordinator),
+            ClashPlayerTownHallLevelSensor(coordinator),
+            ClashPlayerBuilderHallLevelSensor(coordinator),
+            ClashPlayerExperienceLevelSensor(coordinator),
+            ClashPlayerWarStarsSensor(coordinator),
+            ClashPlayerDonationsSensor(coordinator),
+            ClashPlayerDonationsReceivedSensor(coordinator),
         ]
     )
 
 
-class ClashPlayerTrophiesSensor(CoordinatorEntity, SensorEntity):
+class ClashPlayerBaseSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+
+    @property
+    def _player(self):
+        return self.coordinator.data.get("player", {})
+
+    @property
+    def device_info(self):
+        player_name = self._player.get("name")
+        device_name = player_name or f"Clash of Clans ({self.coordinator.player_tag})"
+
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.coordinator.player_tag)},
+            name=device_name,
+            manufacturer="Supercell",
+            model="Clash of Clans",
+        )
+
+
+class ClashPlayerInfoSensor(ClashPlayerBaseSensor):
+    """Sensor for Clash of Clans player info."""
+
+    _attr_name = "Player Info"
+    _attr_icon = "mdi:information"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{coordinator.player_tag}_info"
+
+    @property
+    def native_value(self):
+        return self.coordinator.data["player"]["name"]
+    
+    @property
+    def extra_state_attributes(self):
+        player = self._player
+
+        return {
+            "league": player.get("leagueTier", {}).get("name"),
+            "best_trophies": player.get("bestTrophies"),
+            "builder_hall": player.get("builderHallLevel"),
+            "experience_level": player.get("expLevel"),
+            "clan_capital_contributions": player.get("clanCapitalContributions"),
+            "clan_tag": player.get("clan", {}).get("tag"),
+            "clan_name": player.get("clan", {}).get("name"),
+        }
+
+class ClashPlayerTrophiesSensor(ClashPlayerBaseSensor):
     """Sensor for Clash of Clans player trophies."""
 
-    _attr_name = "Clash of Clans Player Trophies"
+    _attr_name = "Player Trophies"
     _attr_icon = "mdi:trophy"
 
     def __init__(self, coordinator):
         super().__init__(coordinator)
         self._attr_unique_id = f"{coordinator.player_tag}_trophies"
 
-        # Device Info
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, coordinator.player_tag)},
-            name=f"Clash of Clans ({coordinator.player_tag})",
-            manufacturer="Supercell",
-            model="Clash of Clans",
-        )
-
     @property
     def native_value(self):
         return self.coordinator.data["player"]["trophies"]
-    
-    @property
-    def extra_state_attributes(self):
-        player = self.coordinator.data["player"]
 
-        return {
-            "town_hall_level": player.get("townHallLevel"),
-            "xp_level": player.get("expLevel"),
-            "league": player.get("leagueTier", {}).get("name"),
-            "best_trophies": player.get("bestTrophies"),
-            "war_stars": player.get("warStars"),
-        }
+class ClashPlayerTownHallLevelSensor(ClashPlayerBaseSensor):
+    _attr_name = "Town Hall Level"
+    _attr_icon = "mdi:home"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{coordinator.player_tag}_town_hall_level"
+
+    @property
+    def native_value(self):
+        return self._player.get("townHallLevel")
+
+
+class ClashPlayerBuilderHallLevelSensor(ClashPlayerBaseSensor):
+    _attr_name = "Builder Hall Level"
+    _attr_icon = "mdi:home-variant"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{coordinator.player_tag}_builder_hall_level"
+
+    @property
+    def native_value(self):
+        return self._player.get("builderHallLevel")
+
+
+class ClashPlayerExperienceLevelSensor(ClashPlayerBaseSensor):
+    _attr_name = "Experience Level"
+    _attr_icon = "mdi:star"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{coordinator.player_tag}_xp_level"
+
+    @property
+    def native_value(self):
+        return self._player.get("expLevel")
+
+
+class ClashPlayerDonationsSensor(ClashPlayerBaseSensor):
+    _attr_name = "Clash of Clans Player Donations"
+    _attr_icon = "mdi:hand-coin"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{coordinator.player_tag}_donations"
+
+    @property
+    def native_value(self):
+        return self._player.get("donations")
+
+
+class ClashPlayerDonationsReceivedSensor(ClashPlayerBaseSensor):
+    _attr_name = "Clash of Clans Player Donations Received"
+    _attr_icon = "mdi:hand-heart"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{coordinator.player_tag}_donations_received"
+
+    @property
+    def native_value(self):
+        return self._player.get("donationsReceived")
